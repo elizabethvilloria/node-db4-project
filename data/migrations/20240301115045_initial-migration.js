615df5d1c9e42e02b83737afea6efe0ada63779d
elizabethvilloria <lizvilloria@gmail.com>
2024-03-02 10:23:46 +0100

    WIP

--- data/migrations/20240301115045_initial-migration.js
+++ data/migrations/20240301115045_initial-migration.js
@@ -11,7 +11,14 @@ exports.up = async function(knex) {
             table.string('ingredient_unit', 50)
         })
         .createTable('steps', table => {
-            table.increments()
+            table.increments('step_id')
+            table.string('step_text', 200),notNullable()
+            table.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
         })
         .createTable('step_ingredients', table => {
             table.increments()
